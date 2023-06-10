02762311064ea7fed31cd957046b8e24846d8518
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2023-06-10 16:10:13 +0530

    user generation with argon2 hasing function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,6 +16,6 @@ export class AuthController {
     console.log({
       dto
     })
-    return this.authSerive.signin();
+    return this.authSerive.signin(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from "@nestjs/common";
 import { User, Bookmark } from "@prisma/client";
+import { AuthDto } from "src/dto";
 import { PrismaService } from "src/prisma/prisma.service";
+import * as argon from "argon2";
 
 @Injectable({})
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
   signup() {
     return { msg: "this is signup" };
   }
 
-  signin() {
-    return { msg: "this is signin" };
+  async signin(dto : AuthDto) {
+    // generate the password
+    const hash = await argon.hash(dto.password);
+    // save the user
+    const user = await this.prisma.user.create({
+      data:{
+        email : dto.email,
+        hash
+      }
+    })
+    return user;
   }
 }
