0b090d0f9c070c3a4e1445e6a807203e1d20cbc5
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2023-06-11 17:41:22 +0530

    jwtstrategy

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { AuthDto } from "src/dto";
+import { AuthDto } from "src/auth/dto";
 
 @Controller("auth")
 export class AuthController {
